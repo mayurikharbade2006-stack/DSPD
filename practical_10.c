16f3daaf37e331c68bf3799cfa24ebad2e33b939
Mayuri Kharbade <mayurikharbade2006@gmail.com>
2025-11-10 08:33:23 +0530

    Add files via upload

---
+++ practical_10.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *sourceFile, *destFile;
+    char sourceName[50], destName[50];
+    char ch;
+
+    printf("Enter source file name : ");
+    scanf("%s", sourceName);
+
+    printf("Enter destination file name: ");
+    scanf("%s", destName);
+
+    sourceFile = fopen(sourceName, "r");
+    if (sourceFile == NULL) {
+        printf("Error: Cannot open source file '%s'.\n", sourceName);
+        exit(1);
+    }
+
+    destFile = fopen(destName, "w");
+    if (destFile == NULL) {
+        printf("Error: Cannot create destination file '%s'.\n", destName);
+        fclose(sourceFile);
+        exit(1);
+    }
+
+    while ((ch = fgetc(sourceFile)) != EOF) {
+        fputc(ch, destFile);
+    }
+
+    printf("File copied successfully from '%s' to '%s'.\n", sourceName, destName);
+
+    fclose(sourceFile);
+    fclose(destFile);
+
+    return 0;
+}
